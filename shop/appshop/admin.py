ec83d6d35794ed2d31602984d9ab8c23825e2823
midnight <grandcombo258@gmail.com>
2022-05-30 21:39:35 +0400

    First commit

---
+++ shop/appshop/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'city', 'street', 'home_number')
+admin.site.register(City)
+admin.site.register(Schedule)
+admin.site.register(Shop,ShopAdmin)
+admin.site.register(Street)
+
+
---
+++ shop/appshop/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+
+# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=255,verbose_name='Название города')
+
+    def __str__(self):
+        return f'{self.name}'
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+
+class Street(models.Model):
+    name = models.CharField(max_length=255,verbose_name='Название улицы')
+    city = models.ForeignKey(City, on_delete=models.CASCADE,verbose_name='Название города')
+
+    def __str__(self):
+        return f'{self.name}'
+    class Meta:
+        verbose_name = 'Улица'
+        verbose_name_plural = 'Улицы'
+
+
+WORKDAYS = [
+    (1, "Monday"),
+    (2, "Tuesday"),
+    (3, "Wednesday"),
+    (4, "Thursday"),
+    (5, "Friday"),
+]
+
+
+
+class Schedule(models.Model):
+
+    weekday = models.IntegerField(
+        choices=WORKDAYS,
+        unique=True)
+    from_hour = models.TimeField()
+    to_hour = models.TimeField()
+    class Meta:
+        verbose_name = 'Расписание магазина'
+        verbose_name_plural = 'Расписание магазинов'
+
+
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=255,verbose_name='Название магазина')
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    street = models.OneToOneField(Street, on_delete=models.CASCADE)
+    home_number = models.IntegerField(verbose_name='Номер дома')
+    schedule = models.ManyToManyField(Schedule, verbose_name='Расписание магазина')
+
+    def __str__(self):
+        return f'{self.name}'
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
