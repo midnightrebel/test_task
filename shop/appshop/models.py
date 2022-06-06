a24edd54211bbe67428a9fa55a41deaa7287676a
midnight <grandcombo258@gmail.com>
2022-06-06 15:25:20 +0400

    fix filters and added env variables

--- shop/appshop/models.py
+++ shop/appshop/models.py
@@ -1,56 +1,47 @@
 import datetime
 
 from django.db import models
+from django.utils.timezone import localtime
+from rest_framework import request
+from rest_framework.reverse import reverse
 
 
 # Create your models here.
 
-from rest_framework import request
-from rest_framework.reverse import reverse
-from django.utils.timezone import now, localtime
-
-from shop import settings
-
-
 
 class City(models.Model):
-    name = models.CharField(max_length=255,verbose_name='Название города')
+    name = models.CharField(max_length=255, verbose_name='Название города')
 
     def __str__(self):
         return f'{self.name}'
+
     class Meta:
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
 
     def get_absolute_url(self):
-        return reverse('city', kwargs={"city_id": self.pk},request=request)
+        return reverse('city', kwargs={"city_id": self.pk}, request=request)
 
 
 class Street(models.Model):
-    name = models.CharField(max_length=255,verbose_name='Название улицы')
-    city = models.ForeignKey(City, on_delete=models.CASCADE,verbose_name='Название города')
+    name = models.CharField(max_length=255, verbose_name='Название улицы')
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='Название города')
 
     def __str__(self):
         return f'{self.name}'
+
     class Meta:
         verbose_name = 'Улица'
         verbose_name_plural = 'Улицы'
 
 
-
-
-
-
 class Shop(models.Model):
-    name = models.CharField(max_length=255,verbose_name='Название магазина')
-    city = models.ForeignKey(City, on_delete=models.CASCADE,related_name='cities')
+    name = models.CharField(max_length=255, verbose_name='Название магазина')
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='cities')
     street = models.ForeignKey(Street, on_delete=models.CASCADE)
     house_number = models.PositiveIntegerField(verbose_name='Номер дома')
     opening_time = models.TimeField(default=datetime.time(9, 00))
     close_time = models.TimeField(default=datetime.time(18, 00))
-    isOpened = models.BooleanField()
-
-
 
     def __str__(self):
         return f'{self.name}'
@@ -58,20 +49,17 @@ class Shop(models.Model):
     class Meta:
         verbose_name = 'Магазин'
         verbose_name_plural = 'Магазины'
+
     @property
     def cityname(self):
         return self.city.name
+
     def streetname(self):
         return self.street.name
+
     def open(self):
         now = localtime().time()
         if self.opening_time < now < self.close_time:
-            self.isOpened = True
+            return True
         else:
-            self.isOpened = False
-        return self.isOpened
-
-
-
-
-
+            return False
--- shop/appshop/views.py
+++ shop/appshop/views.py
@@ -1,33 +1,28 @@
-from django.db.models import F
-from django.http import JsonResponse
-from django.shortcuts import render
-import datetime
 # Create your views here.
-from django.utils import timezone
-from django.utils.timezone import localtime
-from rest_framework import generics, status
-from rest_framework.exceptions import ValidationError
-from rest_framework.views import APIView
-from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import City, Street, Shop
-from .serializers import CitySerializator,ShopSerializator,StreetSerializer
+from rest_framework import generics, status
+from rest_framework import viewsets
 from rest_framework.response import Response
-from .services import ShopFilter
+
+from .filters import ShopFilter
+from .models import City, Street, Shop
+from .serializers import CitySerializator, ShopSerializator, StreetSerializer
+
 
 class CityAPIView(generics.ListAPIView):
     queryset = City.objects.all()
     serializer_class = CitySerializator
+    filterset_class = ShopFilter
 
 class StreetAPIView(generics.ListAPIView):
     def get_queryset(self):
         return Street.objects.filter(city_id=self.kwargs['city_id']).select_related('city')
+
     queryset = get_queryset
     serializer_class = StreetSerializer
-
+    filterset_class = ShopFilter
 
 class ShopViewSet(viewsets.ModelViewSet):
-
     serializer_class = ShopSerializator
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ShopFilter
@@ -36,34 +31,11 @@ class ShopViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            shop_name = serializer.validated_data['name']
-            city_location = serializer.validated_data['city']
-            street_location = serializer.validated_data['street']
-            now = localtime().time()
-            open_time = serializer.validated_data['opening_time']
-            close_time = serializer.validated_data['close_time']
-            if open_time < now < close_time:
-                serializer.validated_data['isOpened'] = True
-            else:
-                serializer.validated_data['isOpened'] = False
-            if shop_name != '':
-                product_list = Shop.objects.filter(name=shop_name, city=city_location,street= street_location)
-            if not product_list:
-                shop = serializer.save()
-                sid = shop.id
-
-                data = serializer.data
-                data.clear()
-                data.update({'id:': sid})
-                return Response(data, status=status.HTTP_201_CREATED)
-            else:
-                raise ValidationError('Магазин уже существует')
+            shop = serializer.save()
+            sid = shop.id
+            data = serializer.data
+            data.clear()
+            data.update({'id:': sid})
+            return Response(data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
-
-
